656b4d66136578159b95bde84cebf01a7b1c5699
sumit pareek <sumit.pareek@mastercard.com>
2019-07-12 17:12:20 +0530

    pics for theory

--- src/main/java/com/hibernate/hibernate/SampleMAin.java
+++ src/main/java/com/hibernate/hibernate/SampleMAin.java
@@ -3,6 +3,8 @@ package com.hibernate.hibernate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 public class SampleMain {
@@ -10,5 +12,15 @@ public static void main(String[] args) {
 	Set<String> allZoneIds = ZoneId.getAvailableZoneIds();
 	allZoneIds.stream().forEach(x -> System.out.println(x));
 	System.out.println("===== "+ ZonedDateTime.of(LocalDateTime.now(), ZoneId.of("Australia/Sydney")));
+	
+	
+	Map<String, String> map = new HashMap<>();
+	map.put("a", "b");
+	
+	Map<String, String> map1 = new HashMap<>();
+	map1.put("a", "b");
+	
+	map.forEach((k)-> System.out::println);
+	
 }
 }
